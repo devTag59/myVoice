acd0d9ec6dbff58e8358f0c8541ed50e8582dc96
tagDev59 <fernandoKambambi40@gmail.com>
2026-05-30 01:01:45 +0100

    Tailwind configuration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
+      className="flex-1 bg-slate-200 justify-center items-center"
       style={{
         flex: 1,
         justifyContent: "center",
